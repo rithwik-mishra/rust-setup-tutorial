53649b3631e053e8fd9a594343c937f49ef3988d
rmishra05 <rmishra@unc.edu>
2025-01-25 22:55:04 +0000

    Finished Hello Rust tutorial

---
+++ rust_project/target/debug/deps/rust_project-e31d0fc3995b62ac.d
@@ -0,0 +1,5 @@
+/workspaces/rust-setup-tutorial/rust_project/target/debug/deps/rust_project-e31d0fc3995b62ac: src/main.rs
+
+/workspaces/rust-setup-tutorial/rust_project/target/debug/deps/rust_project-e31d0fc3995b62ac.d: src/main.rs
+
+src/main.rs:
---
+++ rust_project/target/debug/rust_project.d
@@ -0,0 +1,1 @@
+/workspaces/rust-setup-tutorial/rust_project/target/debug/rust_project: /workspaces/rust-setup-tutorial/rust_project/src/main.rs
